cd9d5e0e7162a59f8177eea8892ec03c1f58abf4
Ghazanfar-Abbas550 <nda22889@gmail.com>
2025-09-01 16:26:05 +0300

    I am done with task's of module 4

--- Module04/task04.py
+++ Module04/task04.py
@@ -1,19 +1,25 @@
-number = input("Enter number (press Enter to stop): ")
-if number == "":
-    print("No numbers entered.")
-else:
-    number = int(number)
-    smallest = largest = number
+import random
 
-    while True:
-        number_input = input("Enter number (press Enter to stop): ")
-        if number_input == "":
-            break
-        number = int(number_input)
-        if number < smallest:
-            smallest = number
-        if number > largest:
-            largest = number
+# generate a random number between 1 and 10
+secret_number = random.randint(1, 10)
 
-    print("Smallest number:", smallest)
-    print("Largest number:", largest)
+# first guess
+guess = int(input("Guess the number (1–10): "))
+
+# loop until guessed correctly
+while guess != secret_number:
+    if guess < secret_number:
+        print("Too low! Try a higher number.")
+    else:
+        print("Too high! Try a lower number.")
+    guess = int(input("Guess the number (1–10): "))
+
+print("You guessed correctly!")
+
+# Output
+# Guess the number (1–10): 2
+# Too low! Try a higher number.
+# Guess the number (1–10): 4
+# Too high! Try a lower number.
+# Guess the number (1–10): 3
+# You guessed correctly!
